3a88a8fbf936ca2272ab48210d70f76582b13745
Libardo <libardojesusrengifo129@gmail.com>
2022-03-06 17:13:52 -0400

    readme.md

--- views/helpers/utils.mjs
+++ views/helpers/utils.mjs
@@ -1,21 +1,3 @@
-export function convertToSlug(str) {
-  str = str.replace(/^\s+|\s+$/g, "");
-  str = str.toLowerCase();
-
-  var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
-  var to = "aaaaaeeeeeiiiiooooouuuunc------";
-  for (var i = 0, l = from.length; i < l; i++) {
-    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
-  }
-
-  str = str
-    .replace(/[^a-z0-9 -]/g, "")
-    .replace(/\s+/g, "-")
-    .replace(/-+/g, "-");
-
-  return str;
-}
-
 export function readFile(file) {
   const fr = new FileReader();
   return new Promise((resolve, reject) => {
---
+++ views/readme.md
@@ -0,0 +1,9 @@
+# Práctica con Javascript
+
+Proyecto básico de un TodoApp para administrar tareas, programado con vanilla javascript y con SASS, revisar el código fuente para entender.
+
+Demo aquí:
+
+[Notas y tareas (node-projects-znareak.github.io)](https://node-projects-znareak.github.io/mongoose_study/views/)
+
+
